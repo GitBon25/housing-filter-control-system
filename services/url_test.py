9233c2c64de82ae18ed2ac0075a0653ed5abe568
mkulli <egormastugin@gmail.com>
2025-03-31 22:22:40 +1000

    хз

--- services/url_test.py
+++ services/url_test.py
@@ -1,12 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 
-payload = {
-    'sale_price__lte': 10000000,
-    'area__gte': 4,
-    'rooms': 3
-}
-
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
     'Accept': 'application/json, text/plain, */*',
@@ -22,7 +16,12 @@ bs = BeautifulSoup(r.text,"lxml")
 description = bs.find('div',class_='J1rDp').text
 price = bs.find('div',class_='JfVCK').text
 rooms = bs.find('span', {'data-e2e-id': 'Значение'}).text
-description = bs.find('div',class_='J1rDp').text
-description = bs.find('div',class_='J1rDp').text
-description = bs.find('div',class_='J1rDp').text
-print(description,price,rooms)
+area_ = bs.find('li',{'data-e2e-id': 'Площадь'})
+area = area_.find('span',{'data-e2e-id': 'Значение'}).text
+floor_ = bs.find('li',{'data-e2e-id': 'Этаж'})
+floor = floor_.find('span',{'data-e2e-id': 'Значение'}).text
+location = bs.find('a',class_='w9swr').text
+photos_ = bs.find_all('img',class_ = 'picture-image-object-fit--cover-820-3-0-1')
+photos = photos_.get_('src')
+
+print(photos)
